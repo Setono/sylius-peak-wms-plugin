c7f1e3b4965d2274cf69838455dabfbb2f76e0a8
Joachim Løvgaard <joachim@loevgaard.dk>
2024-06-14 15:30:52 +0200

    Add some user friendlyness to the peak wms dashboard inside the admin panel

--- src/Controller/Admin/PeakWMSController.php
+++ src/Controller/Admin/PeakWMSController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Setono\SyliusPeakWMSPlugin\Controller\Admin;
 
+use Setono\PeakWMS\Client\ClientInterface;
 use Setono\SyliusPeakWMSPlugin\Message\Command\RegisterWebhooks;
 use Setono\SyliusPeakWMSPlugin\Registrar\WebhookRegistrarInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,12 +17,22 @@ final class PeakWMSController extends AbstractController
     public function __construct(
         private readonly MessageBusInterface $commandBus,
         private readonly WebhookRegistrarInterface $webhookRegistrar,
+        private readonly ClientInterface $client,
     ) {
     }
 
     public function index(): Response
     {
+        $apiError = false;
+
+        try {
+            $this->client->ping();
+        } catch (\Throwable) {
+            $apiError = 'There was an error connecting to the API. Please check your credentials and try again. If you continue to experience issues, did you forget to enable/disable the sandbox?';
+        }
+
         return $this->render('@SetonoSyliusPeakWMSPlugin/admin/peak_wms/index.html.twig', [
+            'apiError' => $apiError,
             'webhooksShouldBeRegistered' => $this->webhookRegistrar->outOfDate(),
         ]);
     }
