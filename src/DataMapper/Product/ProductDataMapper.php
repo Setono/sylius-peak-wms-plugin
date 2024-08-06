a9484f450172db163ef791b2141345fdcb66e600
Joachim Løvgaard <joachim@loevgaard.dk>
2024-08-06 22:01:41 +0200

    Add product data mapper

---
+++ src/DataMapper/Product/ProductDataMapper.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusPeakPlugin\DataMapper\Product;
+
+use Setono\PeakWMS\DataTransferObject\Product\Product;
+use Setono\SyliusPeakPlugin\Model\ProductVariantInterface;
+
+final class ProductDataMapper implements ProductDataMapperInterface
+{
+    public function map(ProductVariantInterface $productVariant, Product $product): void
+    {
+        $product->productId = $productVariant->getProduct()?->getCode();
+        $product->variantId = $productVariant->getCode();
+        $product->itemNumber = $productVariant->getCode();
+    }
+}
