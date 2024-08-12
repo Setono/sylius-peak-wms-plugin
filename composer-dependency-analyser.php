fc0b93cefd69fc4070e386fe26768081b453b670
Joachim Løvgaard <joachim@loevgaard.dk>
2024-08-12 10:09:13 +0200

    Add false positive

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -1,7 +1,9 @@
 <?php
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
+    ->ignoreErrorsOnPackage('sylius/order', ErrorType::UNUSED_DEPENDENCY) // is used in our doctrine mapping
 ;
