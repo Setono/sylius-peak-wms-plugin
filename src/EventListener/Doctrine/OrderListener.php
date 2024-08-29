a618b0ff307ee5bd98c975ff6a157b1b4cc23d4d
Joachim Løvgaard <joachim@loevgaard.dk>
2024-08-29 13:12:28 +0200

    Add order listener

---
+++ src/EventListener/Doctrine/OrderListener.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusPeakPlugin\EventListener\Doctrine;
+
+use Doctrine\Persistence\Event\PreUpdateEventArgs;
+use Setono\SyliusPeakPlugin\Factory\UploadOrderRequestFactoryInterface;
+use Setono\SyliusPeakPlugin\Model\OrderInterface;
+use Setono\SyliusPeakPlugin\Workflow\UploadOrderRequestWorkflow;
+use Symfony\Component\Workflow\WorkflowInterface;
+
+final class OrderListener
+{
+    public function __construct(
+        private readonly UploadOrderRequestFactoryInterface $uploadOrderRequestFactory,
+        private readonly WorkflowInterface $uploadOrderRequestWorkflow,
+    ) {
+    }
+
+    public function preUpdate(PreUpdateEventArgs $eventArgs): void
+    {
+        $obj = $eventArgs->getObject();
+        if (!$obj instanceof OrderInterface) {
+            return;
+        }
+
+        // For now this listener only triggers when the state changes
+        if (!$eventArgs->hasChangedField('state')) {
+            return;
+        }
+
+        $uploadOrderRequest = $obj->getPeakUploadOrderRequest() ?? $this->uploadOrderRequestFactory->createNew();
+        $this->uploadOrderRequestWorkflow->apply($uploadOrderRequest, UploadOrderRequestWorkflow::TRANSITION_RESET);
+
+        $obj->setPeakUploadOrderRequest($uploadOrderRequest);
+    }
+}
